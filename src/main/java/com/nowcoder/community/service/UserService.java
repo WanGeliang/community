ea450d44da18fc380a1ba02bb1e409ff50772d0e
wgl0515@163.com <wgl0515@163.com>
2021-01-29 18:53:42 +0800

    开发了登录界面和注册界面

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,8 +2,21 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static com.nowcoder.community.util.CommunityConstant.*;
 
 @Service
 public class UserService {
@@ -11,8 +24,106 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    MailClient mailClient;
+
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    String domain;
+
+    @Value("${server.servlet.context-path}")
+    String path;
+
+    /**
+     * 通过用户id查询user
+     *
+     * @param id
+     * @return
+     */
     public User findUserById(int id) {
         return userMapper.selectById(id);
+    }
+
+    /**
+     * 进行注册
+     *
+     * @param user
+     * @return 返回到集合的页面的信息
+     */
+    public Map<String, Object> register(User user) {
+        HashMap<String, Object> map = new HashMap<>();
+        if (user == null) {
+            throw new IllegalArgumentException("参数不能为空!");
+        }
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "输入的账号为空");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "输入的密码为空");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "输入的邮箱为空");
+        }
+        //不为空时，验证用户名
+        User u = userMapper.selectByName(user.getUsername());
+        if (u != null) {
+            map.put("usernameMsg", "该账号已存在!");
+            return map;
+        }
+        //不为空时，验证邮箱
+        u = userMapper.selectByEmail(user.getEmail());
+        if (u != null) {
+            map.put("emailMsg", "该邮箱已被注册过!");
+            return map;
+        }
+
+        // 注册用户
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        //像数据库中插入用户信息
+        userMapper.insertUser(user);
+
+        //进行激活邮件+
+        //利用Theameleaf进行传入
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        String url = domain + path + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        //利用模板引擎将邮箱，和地址传入到前端
+        String content = templateEngine.process("/mail/activation", context);
+        //使用之前定义好的工具类jing'xing发送邮件
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
+
+
+        return map;
+    }
+
+    /**
+     * 根据用户id和code值判断激活的状态
+     * 只能激活一次，不能重复激活
+     * @param userId
+     * @param code
+     * @return
+     */
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if (user.getStatus() == 1) {
+            return ACTIVATION_REPEAT;
+        } else if (user.getActivationCode().equals(code)) {
+            userMapper.updateStatus(userId, 1);
+            return ACTIVATION_SUCCESS;
+        } else {
+            return ACTIVATION_FAILURE;
+        }
     }
 
 }
