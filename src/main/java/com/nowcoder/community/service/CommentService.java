88ba1c5462324d806c628f71ab063826d6134131
wgl0515@163.com <wgl0515@163.com>
2021-02-03 19:58:44 +0800

    开发了显示帖子功能 开发了显示展开帖子功能

---
+++ src/main/java/com/nowcoder/community/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.CommentMapper;
+import com.nowcoder.community.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    CommentMapper commentMapper;
+
+    //实现帖子的分页查询
+    public List<Comment> findCommentByEntity(int entityType, int entityId, int offset, int limit) {
+        return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
+    }
+
+    //查询帖子的数量
+    public int findCommentCount(int entityType, int entityId) {
+        return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+}
